bc16f5d21ff3f3e1dea92b3efb8c93deafe8c774
chenglei <282449144@qq.com>
2021-08-30 17:58:24 +0800

    1

---
+++ service-core/src/main/java/com/cl/srb/core/ServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.cl.srb.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author chenglei
+ * @create 2021-08-30 17:53
+ */
+@SpringBootApplication
+@ComponentScan("com.cl.srb")
+public class ServiceCoreApplication {
+}
